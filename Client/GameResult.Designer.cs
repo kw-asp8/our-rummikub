cce9750379b579595cb52efa0b7cf54b31642d61
mingming0508 <minjuyoubin@naver.com>
2019-05-06 16:53:48 +0900

    GameResult form 생성, Game result 창과 Game 화면 연결, timer 작동

---
+++ Client/GameResult.Designer.cs
@@ -0,0 +1,306 @@
+﻿namespace Client
+{
+    partial class GameResult
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_exit = new System.Windows.Forms.Label();
+            this.lbl_result = new System.Windows.Forms.Label();
+            this.lbl_1 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbl_first = new System.Windows.Forms.Label();
+            this.lbl_second = new System.Windows.Forms.Label();
+            this.lbl_third = new System.Windows.Forms.Label();
+            this.lbl_forth = new System.Windows.Forms.Label();
+            this.lbl_first_score = new System.Windows.Forms.Label();
+            this.lbl_second_score = new System.Windows.Forms.Label();
+            this.lbl_third_score = new System.Windows.Forms.Label();
+            this.lbl_forth_score = new System.Windows.Forms.Label();
+            this.btn_regame = new System.Windows.Forms.Button();
+            this.btn_main = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl_exit
+            // 
+            this.lbl_exit.AutoSize = true;
+            this.lbl_exit.Dock = System.Windows.Forms.DockStyle.Right;
+            this.lbl_exit.Font = new System.Drawing.Font("Microsoft JhengHei UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_exit.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.lbl_exit.Location = new System.Drawing.Point(619, 0);
+            this.lbl_exit.Name = "lbl_exit";
+            this.lbl_exit.Size = new System.Drawing.Size(34, 36);
+            this.lbl_exit.TabIndex = 10;
+            this.lbl_exit.Text = "X";
+            this.lbl_exit.Click += new System.EventHandler(this.lbl_exit_Click);
+            this.lbl_exit.MouseLeave += new System.EventHandler(this.lbl_exit_MouseLeave);
+            this.lbl_exit.MouseMove += new System.Windows.Forms.MouseEventHandler(this.lbl_exit_MouseMove);
+            // 
+            // lbl_result
+            // 
+            this.lbl_result.AutoSize = true;
+            this.lbl_result.Font = new System.Drawing.Font("Cambria", 24F);
+            this.lbl_result.ForeColor = System.Drawing.Color.White;
+            this.lbl_result.Location = new System.Drawing.Point(217, 67);
+            this.lbl_result.Name = "lbl_result";
+            this.lbl_result.Size = new System.Drawing.Size(234, 47);
+            this.lbl_result.TabIndex = 11;
+            this.lbl_result.Text = "Game Result";
+            // 
+            // lbl_1
+            // 
+            this.lbl_1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbl_1.AutoSize = true;
+            this.lbl_1.Font = new System.Drawing.Font("Cambria", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_1.ForeColor = System.Drawing.Color.White;
+            this.lbl_1.Location = new System.Drawing.Point(145, 150);
+            this.lbl_1.Name = "lbl_1";
+            this.lbl_1.Size = new System.Drawing.Size(42, 38);
+            this.lbl_1.TabIndex = 12;
+            this.lbl_1.Text = "1.";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Cambria", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(145, 210);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 38);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "2.";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Cambria", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(145, 270);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 38);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "3.";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Cambria", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(145, 329);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 38);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "4.";
+            // 
+            // lbl_first
+            // 
+            this.lbl_first.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbl_first.AutoSize = true;
+            this.lbl_first.Font = new System.Drawing.Font("Cambria", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_first.ForeColor = System.Drawing.Color.White;
+            this.lbl_first.Location = new System.Drawing.Point(203, 150);
+            this.lbl_first.Name = "lbl_first";
+            this.lbl_first.Size = new System.Drawing.Size(0, 38);
+            this.lbl_first.TabIndex = 16;
+            // 
+            // lbl_second
+            // 
+            this.lbl_second.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbl_second.AutoSize = true;
+            this.lbl_second.Font = new System.Drawing.Font("Cambria", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_second.ForeColor = System.Drawing.Color.White;
+            this.lbl_second.Location = new System.Drawing.Point(203, 210);
+            this.lbl_second.Name = "lbl_second";
+            this.lbl_second.Size = new System.Drawing.Size(0, 38);
+            this.lbl_second.TabIndex = 17;
+            // 
+            // lbl_third
+            // 
+            this.lbl_third.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbl_third.AutoSize = true;
+            this.lbl_third.Font = new System.Drawing.Font("Cambria", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_third.ForeColor = System.Drawing.Color.White;
+            this.lbl_third.Location = new System.Drawing.Point(203, 270);
+            this.lbl_third.Name = "lbl_third";
+            this.lbl_third.Size = new System.Drawing.Size(0, 38);
+            this.lbl_third.TabIndex = 18;
+            // 
+            // lbl_forth
+            // 
+            this.lbl_forth.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbl_forth.AutoSize = true;
+            this.lbl_forth.Font = new System.Drawing.Font("Cambria", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_forth.ForeColor = System.Drawing.Color.White;
+            this.lbl_forth.Location = new System.Drawing.Point(203, 329);
+            this.lbl_forth.Name = "lbl_forth";
+            this.lbl_forth.Size = new System.Drawing.Size(0, 38);
+            this.lbl_forth.TabIndex = 19;
+            // 
+            // lbl_first_score
+            // 
+            this.lbl_first_score.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbl_first_score.AutoSize = true;
+            this.lbl_first_score.Font = new System.Drawing.Font("Cambria", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_first_score.ForeColor = System.Drawing.Color.White;
+            this.lbl_first_score.Location = new System.Drawing.Point(457, 150);
+            this.lbl_first_score.Name = "lbl_first_score";
+            this.lbl_first_score.Size = new System.Drawing.Size(35, 38);
+            this.lbl_first_score.TabIndex = 20;
+            this.lbl_first_score.Text = "0";
+            // 
+            // lbl_second_score
+            // 
+            this.lbl_second_score.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbl_second_score.AutoSize = true;
+            this.lbl_second_score.Font = new System.Drawing.Font("Cambria", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_second_score.ForeColor = System.Drawing.Color.White;
+            this.lbl_second_score.Location = new System.Drawing.Point(457, 210);
+            this.lbl_second_score.Name = "lbl_second_score";
+            this.lbl_second_score.Size = new System.Drawing.Size(35, 38);
+            this.lbl_second_score.TabIndex = 21;
+            this.lbl_second_score.Text = "0";
+            // 
+            // lbl_third_score
+            // 
+            this.lbl_third_score.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbl_third_score.AutoSize = true;
+            this.lbl_third_score.Font = new System.Drawing.Font("Cambria", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_third_score.ForeColor = System.Drawing.Color.White;
+            this.lbl_third_score.Location = new System.Drawing.Point(457, 270);
+            this.lbl_third_score.Name = "lbl_third_score";
+            this.lbl_third_score.Size = new System.Drawing.Size(35, 38);
+            this.lbl_third_score.TabIndex = 22;
+            this.lbl_third_score.Text = "0";
+            // 
+            // lbl_forth_score
+            // 
+            this.lbl_forth_score.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbl_forth_score.AutoSize = true;
+            this.lbl_forth_score.Font = new System.Drawing.Font("Cambria", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_forth_score.ForeColor = System.Drawing.Color.White;
+            this.lbl_forth_score.Location = new System.Drawing.Point(457, 329);
+            this.lbl_forth_score.Name = "lbl_forth_score";
+            this.lbl_forth_score.Size = new System.Drawing.Size(35, 38);
+            this.lbl_forth_score.TabIndex = 23;
+            this.lbl_forth_score.Text = "0";
+            // 
+            // btn_regame
+            // 
+            this.btn_regame.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(43)))), ((int)(((byte)(180)))));
+            this.btn_regame.FlatAppearance.BorderSize = 0;
+            this.btn_regame.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_regame.Font = new System.Drawing.Font("Georgia", 17F);
+            this.btn_regame.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn_regame.Location = new System.Drawing.Point(122, 409);
+            this.btn_regame.Name = "btn_regame";
+            this.btn_regame.Size = new System.Drawing.Size(138, 62);
+            this.btn_regame.TabIndex = 24;
+            this.btn_regame.Text = "Regame";
+            this.btn_regame.UseVisualStyleBackColor = false;
+            // 
+            // btn_main
+            // 
+            this.btn_main.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(43)))), ((int)(((byte)(180)))));
+            this.btn_main.FlatAppearance.BorderSize = 0;
+            this.btn_main.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_main.Font = new System.Drawing.Font("Georgia", 17F);
+            this.btn_main.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn_main.Location = new System.Drawing.Point(377, 409);
+            this.btn_main.Name = "btn_main";
+            this.btn_main.Size = new System.Drawing.Size(138, 62);
+            this.btn_main.TabIndex = 25;
+            this.btn_main.Text = "Main";
+            this.btn_main.UseVisualStyleBackColor = false;
+            // 
+            // GameResult
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(120)))), ((int)(((byte)(240)))));
+            this.ClientSize = new System.Drawing.Size(653, 531);
+            this.Controls.Add(this.btn_main);
+            this.Controls.Add(this.btn_regame);
+            this.Controls.Add(this.lbl_forth_score);
+            this.Controls.Add(this.lbl_third_score);
+            this.Controls.Add(this.lbl_second_score);
+            this.Controls.Add(this.lbl_first_score);
+            this.Controls.Add(this.lbl_forth);
+            this.Controls.Add(this.lbl_third);
+            this.Controls.Add(this.lbl_second);
+            this.Controls.Add(this.lbl_first);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbl_1);
+            this.Controls.Add(this.lbl_result);
+            this.Controls.Add(this.lbl_exit);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "GameResult";
+            this.Text = "GameResult";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.GameResult_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.GameResult_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_exit;
+        private System.Windows.Forms.Label lbl_result;
+        private System.Windows.Forms.Label lbl_1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbl_first;
+        private System.Windows.Forms.Label lbl_second;
+        private System.Windows.Forms.Label lbl_third;
+        private System.Windows.Forms.Label lbl_forth;
+        private System.Windows.Forms.Label lbl_first_score;
+        private System.Windows.Forms.Label lbl_second_score;
+        private System.Windows.Forms.Label lbl_third_score;
+        private System.Windows.Forms.Label lbl_forth_score;
+        private System.Windows.Forms.Button btn_regame;
+        private System.Windows.Forms.Button btn_main;
+    }
+}
